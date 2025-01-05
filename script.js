390b7d7f898f122ae940b9db09fe592afc33a600
xXSnowXx <wishitweresnowing@gmail.com>
2025-01-04 21:37:22 -0800

    let chatGPT have fun with an anime themed redesign cuz i didn't have much time today, probably borked untested functionality. nonetheless i am really happy with this one LOL

--- script.js
+++ script.js
@@ -9,17 +9,5 @@ chatButton.addEventListener('click', async () => {
     chatBox.innerHTML += `<p><strong>You:</strong> ${userMessage}</p>`;
     userInput.value = '';
 
-    try {
-        const response = await fetch('http://localhost:3000/chat', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ message: userMessage }),
-        });
-
-        const data = await response.json();
-        chatBox.innerHTML += `<p><strong>Bot:</strong> ${data.reply}</p>`;
-    } catch (error) {
-        chatBox.innerHTML += `<p><strong>Error:</strong> Failed to fetch response.</p>`;
-        console.error(error);
-    }
+    chatBox.innerHTML += `<p><strong>Bot:</strong> Onii-chan, the backend isn't ready yet... 💬</p>`;
 });
